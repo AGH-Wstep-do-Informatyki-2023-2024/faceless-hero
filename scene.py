580898e51c499483c6fa2249615fb872efd0d9be
Guest User <Guest@Mac-mini-8.local>
2023-12-13 18:01:16 +0100

    parallax

--- scene.py
+++ scene.py
@@ -2,6 +2,7 @@ from interval import Interval
 from group import Group
 from globals import *
 import pygame as pg
+from player import Player
 
 class Scene:
     def __init__(self, game):
@@ -13,18 +14,18 @@ class Scene:
         self.entities.update()
 
     def draw_bg(self):
-        self.scroll = 5
         self.bg_images = []
         
         for i in range(3, -1, -1):
             self.bg_image = pg.image.load(f"assets/background/background paralax_{i}.png").convert_alpha()
+            self.bg_image = pg.transform.scale(self.bg_image,(640, 480))
             self.bg_images.append(self.bg_image)
         
         self.bg_width = self.bg_images[0].get_width()
 
         for x in range(5):
             for i in self.bg_images:
-                self.game.screen.blit(i, ((x * self.bg_width) - self.scroll, 0))
+                self.game.screen.blit(i, ((x * self.bg_width), 0))
 
     # def draw(self):
     #     self.game.screen.fill("crimson")
