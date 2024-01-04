b33fbeca89994362bd9993f34630dd382ae3389a
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2024-01-04 11:45:12 +0100

    use pygame.time.get_ticks instead of threading.Timer for autosave

--- scene.py
+++ scene.py
@@ -1,21 +1,23 @@
 import pygame as pg
-from interval import Interval
 from group import Group
+from timer import Timer
 from globals import *
 
 MAP_COLOR = (152, 166, 212)
 
-AUTOSAVE_INTERVAL = 60
+AUTOSAVE_INTERVAL = 60 * 1000
 
 
 class Scene:
     def __init__(self, screen: pg.Surface):
         self.screen = screen
         self.group = Group(screen)
-        self.autosave = Interval(AUTOSAVE_INTERVAL, self.group.save)
+        self.autosave = Timer(AUTOSAVE_INTERVAL, True, self.group.save)
+        self.autosave.start()
 
     def update(self):
         self.group.update()
+        self.autosave.update()
 
     def draw(self):
         self.screen.fill(MAP_COLOR)
